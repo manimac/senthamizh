cd4ccce214821913599161278220ba7c4737f264
manimac <manimaccse@gmail.com>
2020-02-15 15:55:23 +0530

    15-2-2020

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,14 +3,30 @@ import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
 import { Home } from './home-component/home-component.component';
+import { AboutComponent } from './about/about.component';
+
+import { Routes, RouterModule} from '@angular/router';
+import{ FormsModule} from '@angular/forms';
+
+const route: Routes = [
+  {
+    path: 'home', component: Home
+  },
+  {
+    path: 'about', component: AboutComponent
+  }
+]
 
 @NgModule({
   declarations: [
     AppComponent,
-    Home
+    Home,
+    AboutComponent
   ],
   imports: [
     BrowserModule,
+    RouterModule.forRoot(route),
+    FormsModule
   ],
   providers: [],
   bootstrap: [AppComponent]
--- src/app/home-component/home-component.component.ts
+++ src/app/home-component/home-component.component.ts
@@ -1,15 +1,31 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, AfterViewInit } from '@angular/core';
 
 @Component({
   selector: 'app-home-component',
   templateUrl: './home-component.component.html',
   styleUrls: ['./home-component.component.css']
 })
-export class Home implements OnInit {
+export class Home {
+
+  val: any;
+  private studentName: string = '<b>Rajiv</b>';
+  showDiv = false;
 
   constructor() { }
 
   ngOnInit() {
+  }
+
+  ngAfterViewInit(){
+
+  }
+
+  toggleDiv(){
+    this.showDiv = !this.showDiv;
+  }
+
+  checkVal(param){
+    this.val = param;
   }
 
 }
